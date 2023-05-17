b8eb11420775984db69566bf70b40c99b3b9fa92
mudit <mudit.sethi@assaabloy.com>
2023-05-17 11:20:12 +0200

    Commented out application deploy!

--- infra/lib/axon-rds-stack.ts
+++ infra/lib/axon-rds-stack.ts
@@ -32,31 +32,35 @@ export class AxonRdsStack extends cdk.Stack {
             instanceProps: {vpc: vpc},
             defaultDatabaseName: 'axon'
         })
-        const loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, 'axon-rds-test-service', {
-                vpc: vpc,
-                memoryLimitMiB: 1024,
-                cpu: 512,
-                desiredCount: 5,
-                taskImageOptions: {
-                    image: ContainerImage.fromEcrRepository(ecrRepo, "axon-dynamodb"),
-                    environment: {},
-                    containerPort: 8080
-                }
-            }
-        );
-        loadBalancedFargateService.taskDefinition.addToTaskRolePolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            resources: [databaseSecret.secretArn],
-            actions: ["secretsmanager:DescribeSecret",
-                "secretsmanager:GetResourcePolicy",
-                "secretsmanager:GetSecretValue",
-                "secretsmanager:ListSecretVersionIds"]
-        }))
+    //     const loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, 'axon-rds-test-service', {
+    //             vpc: vpc,
+    //             memoryLimitMiB: 1024,
+    //             cpu: 512,
+    //             desiredCount: 5,
+    //             taskImageOptions: {
+    //                 image: ContainerImage.fromEcrRepository(ecrRepo, "axon-dynamodb"),
+    //                 environment: {
+    //                     "SERVICE_DATABASE_PROXY_URL": cluster.clusterEndpoint.hostname,
+    //                     "SERVICE_DATABASE_SECRET": databaseSecret.secretArn
+    //                 },
+    //                 containerPort: 8080
+    //             }
+    //         }
+    //     );
+    //     loadBalancedFargateService.taskDefinition.addToTaskRolePolicy(new PolicyStatement({
+    //         effect: Effect.ALLOW,
+    //         resources: [databaseSecret.secretArn],
+    //         actions: ["secretsmanager:DescribeSecret",
+    //             "secretsmanager:GetResourcePolicy",
+    //             "secretsmanager:GetSecretValue",
+    //             "secretsmanager:ListSecretVersionIds"]
+    //     }))
+    //
+    //         loadBalancedFargateService.service.connections.allowToDefaultPort(cluster)
+    //         loadBalancedFargateService.targetGroup.configureHealthCheck({
+    //             path: "/health",
+    //             port: "8080"
+    //         })
 
-            loadBalancedFargateService.service.connections.allowToDefaultPort(cluster)
-            loadBalancedFargateService.targetGroup.configureHealthCheck({
-                path: "/health",
-                port: "8080"
-            })
     }
 }
